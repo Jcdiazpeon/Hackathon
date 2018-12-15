828674b85dce822b7b74dafb81a9940343e7ce2e
Cornchipss <cornchipgonecodin@gmail.com>
2018-12-15 13:01:43 -0500

    Perfecting it

--- js/weather.js
+++ js/weather.js
@@ -1,53 +1,60 @@
 function getCoordinates(callback)
 {
-    const API_KEY = '19f72ad7d0e5911653d8e37df08280d7';
+    let debug = false;
 
-    // Fill coordinates
-    $.getJSON('/data/counties.json', function(data)
+    if(debug)
     {
-        let coordinates = [];
+        $.ajax({
+            url: '/data/weather.json',
+            dataType: 'jsonp',
+            success: function (data) {
+                callback(JSON.parse('data'));
+            }
+        });
+    }
+    else
+    {
+        const API_KEY = '19f72ad7d0e5911653d8e37df08280d7';
 
-        let url = `https://api.darksky.net/forecast/${API_KEY}`;
+        // Fill coordinates
+        $.getJSON('/data/counties.json', function(data)
+        {
+            let coordinates = [];
 
-        let lat, lon;
-/*
-0: Object { name: "Adams County", latitude: 3.046, longitude: 39.869471 }
-​
-1: Object { name: "Allegheny County", latitude: 14.445, longitude: 40.469757 }
-​
-2: Object { name: "Armstrong County", latitude: 10.666, longitude: 40.81238 }
-​
-3: Object { name: "Beaver County", latitude: 9.331, longitude: 40.68414 }
-​
-4: Object { name: "Bedford County", latitude: 4.608, longitude: 39.99862 }
-*/
-        let gotten = 0;
+            let url = `https://api.darksky.net/forecast/${API_KEY}`;
 
-        for(let x = 0; x < data.length; x++)
-        {            
-            $.ajax(
+            let lat, lon;
+
+            let gotten = 0;
+
+            for(let x = 0; x < data.length; x++)
             {
-                method: 'GET',
-                url: `${url}/${data[x].latitude},${data[x].longitude}`,
-                dataType: 'jsonp',
-                data: {
-                    format: "json"
-                },
-                success: (res) =>
+                $.ajax(
                 {
-                    lat = data[x].latitude;
-                    lon = data[x].longitude;
+                    method: 'GET',
+                    url: `${url}/${data[x].latitude},${data[x].longitude}`,
+                    dataType: 'jsonp',
+                    data: {
+                        format: "json"
+                    },
+                    success: (res) =>
+                    {
+                        lat = data[x].latitude;
+                        lon = data[x].longitude;
 
-                    coordinates.push(new Coordinate(lat, lon, res));
-                    // ex: coordinates.push(new Coordinate(lat, lon, weather data extracted from res));
+                        coordinates.push(new Coordinate(lat, lon, res));
+                        // ex: coordinates.push(new Coordinate(lat, lon, weather data extracted from res));
 
-                    gotten++;
-                    if(gotten === data.length)
-                        callback(coordinates);
-                }
-            }); 
-        }
-    });
+                        gotten++;
+                        if(gotten === data.length)
+                        {
+                            callback(coordinates);
+                        }
+                    }
+                });
+            }
+        });
+    }
 }
 
 class Coordinate
